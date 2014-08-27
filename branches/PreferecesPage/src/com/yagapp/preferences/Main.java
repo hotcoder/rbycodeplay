e86811162cb2a606d30b370c9b599716fa176af5
rby <rby@15b9e9f6-d7b8-4abf-9c34-17cdab12d3eb>
2014-08-27 15:42:37 +0000

    Preference Page - a simple programme contains the logic to give pages based on query and pages words combination criteria

---
+++ branches/PreferecesPage/src/com/yagapp/preferences/Main.java
@@ -0,0 +1,14 @@
+package com.yagapp.preferences;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		FetchPagesByQueryKeys f = new FetchPagesByQueryKeys();
+		f.startSearch();
+
+	}
+
+}
